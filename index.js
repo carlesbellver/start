b31948c262bbc64f1e2f4002b3ffd1026c98ee6e
Carles Bellver Torlà <carles.bellver@gmail.com>
2020-08-04 12:23:17 +0200

    Change day period hours according to time of year

--- index.js
+++ index.js
@@ -38,17 +38,72 @@ document.addEventListener('DOMContentLoaded', function (event) {
     const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
     const w = now.getDay()
     const day = days[w]
+    const m = now.getMonth()
+    
+    afternoon = 15
+    if (m == 0) {
+      morning = 8
+      night = 18
+    }
+    else if (m == 1) {
+      morning = 8
+      night = 18
+    }
+    else if (m == 2) {
+      morning = 7
+      night = 19
+    }
+    else if (m == 3) {
+      morning = 7
+      night = 20
+    }
+    else if (m == 4) {
+      morning = 7
+      night = 21
+    }
+    else if (m == 5) {
+      morning = 6
+      night = 21
+    }
+    else if (m == 6) {
+      morning = 6
+      night = 21
+    }
+    else if (m == 7) {
+      morning = 7
+      night = 21
+    }
+    else if (m == 8) {
+      morning = 7
+      night = 20
+    }
+    else if (m == 9) {
+      morning = 8
+      night = 19
+    }
+    else if (m == 10) {
+      morning = 8
+      night = 18
+    }
+    else if (m == 11) {
+      morning = 8
+      night = 18
+    }
+    else if (m == 1) {
+      morning = 8
+      night = 18
+    }
 
     const greetings = document.querySelector('.content-greeting > h1')
 
     let period
 
-    if (h >= 5 && h < 12) period = 1
-    else if (h >= 12 && h < 18) period = 2
+    if (h >= morning && h < afternoon) period = 1
+    else if (h >= afternoon && h < night) period = 2
     else period = 0
 
     if (period === 1) {
-      greetings.innerHTML = 'Good morning. It\'s ' + day + '.'
+      greetings.innerHTML = 'Good morning. It\’s ' + day + '.'
       document.querySelector(':root').style.setProperty('--background', '#DCEDFF')
       document.querySelector(':root').style.setProperty('--input', '#fff')
       document.querySelector(':root').style.setProperty('--link', '#4A5E6D')
@@ -56,14 +111,14 @@ document.addEventListener('DOMContentLoaded', function (event) {
       document.querySelector(':root').style.setProperty('--color', '#232C33')
     }
     if (period === 2) {
-      greetings.innerHTML = 'Good afternoon. It\'s ' + day + '.'
+      greetings.innerHTML = 'Good afternoon. It\’s ' + day + '.'
       document.querySelector(':root').style.setProperty('--background', '#97C3B6')
       document.querySelector(':root').style.setProperty('--input', '#F9E7E7')
       document.querySelector(':root').style.setProperty('--color', '#0A2E36')
       document.querySelector(':root').style.setProperty('--shadow', '#0A2E36')
     }
     if (period === 0) {
-      greetings.innerHTML = 'Good evening. It\'s still ' + day + '.'
+      greetings.innerHTML = 'Good evening. It\’s still ' + day + '.'
       document.querySelector(':root').style.setProperty('--background', '#161212')
       document.querySelector(':root').style.setProperty('--input', '#7B6565')
       document.querySelector(':root').style.setProperty('--color', '#F9E7E7')
